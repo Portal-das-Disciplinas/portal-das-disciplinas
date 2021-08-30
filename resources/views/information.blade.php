896678c8a173ef58e330e1c038ea20fb90143cb5
Cristian <crircyt@gmail.com>
2021-08-30 18:51:26 -0300

    sobre

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -6,33 +6,94 @@ Sobre nós - Portal das Disciplinas IMD
 
 @section('content')
 
-<div class="row info-portal">
-    <div class="col-12 ">
-        <h3>O que é o portal da disciplina?</h3>
+<div class='discipline-banner text-center d-flex align-items-center justify-content-center text-white'>
+    <h1>Sobre & Colabore</h1>
+</div>
+
+<div class='mt-8 container text-white'  id="top-container">
+    <div class='row'>
+        <div class="col-md-5">
+                <div>
+                    <h2>Nossa equipe</h2>
+                    <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eveniet temporibus dolorum facere ullam repellat doloribus architecto tempore unde quae? Tempore cumque dolor velit enim aliquam laboriosam, dignissimos doloremque odio? Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+
+                <div>
+                    <h2>Colabore</h2>
+                    <p class="text-justify">Caso tenha interesse em colaborar na adição de novas funcionalidades do site como sistema de classificações dinâmicos, refinamento de mecanismos de busca, interação com o portal de dados abertos da UFRN para recuperação de índices de aprovação de disciplinas, implementação de fóruns no portal, entre outros, por favor, entre em contato conosco.</p>
+                </div>
+
+                <div class="break-word">
+                    <b>Lista de emails para contato</b>
+                    <ul class="ml-3">
+                        <li>eugenio@imd.ufrn.br</li>
+                        <li>pedrogab96@gmail.com</li>
+                        <li>victor_brandao@outlook.com</li>
+                    </ul>
+                </div>
+        </div>
+
+
         
-        <p class="mt-4 span-info text-justify">
-            Este portal tem por intuito promover informações pertinentes para que os estudantes conheçam com maior profundidade a natureza das 
-            disciplinas do Instituto Metrópole Digital, de modo a terem um maior embasamento para suas escolhas curriculares a cada semestre. Basicamente, 
-            tentamos responder à pergunta "e essa disciplina aí, como é?"
-        </p>
+        <div id="devsGrid" class="col-md-7 d-flex flex-column align-items-center">
+            <div class="row">
+                <img class="fluid clip-path" src="" alt="foto-dev">
+                    <div class="text-center">
+                        <h1 style="font-size: 1.4rem" class="mb-0">Eugênio Paccelli</h1>
+                        <p>Professor Orientador</p>
+                    </div>
+            </div>
+
+            <div class="d-flex flex-wrap justify-content-around mt-4">
+                
+                <div class="d-flex flex-column justify-content-center align-items-center mx-4">
+                        <img class="fluid clip-path" src="" alt="foto-dev">
+                            <div class="text-center">
+                                <h1 style="font-size: 1.4rem" class="mb-0">IMD Dev</h1>
+                                <p>Desenvolvedor <code>back-end</code></p>
+                            </div>
+                        
+                        <img class="fluid clip-path" src="" alt="foto-dev">
+                            <div class="text-center">
+                                <h1 style="font-size: 1.4rem" class="mb-0">IMD Dev</h1>
+                                <p>Desenvolvedor <code>back-end</code></p>
+                            </div>
+
+                        <img class="fluid clip-path" src="" alt="foto-dev">
+                            <div class="text-center">
+                                <h1 style="font-size: 1.4rem" class="mb-0">IMD Dev</h1>
+                                <p>Desenvolvedor <code>back-end</code></p>
+                            </div>
+
+                        <img class="fluid clip-path" src="" alt="foto-dev">
+                            <div class="text-center">
+                                <h1 style="font-size: 1.4rem" class="mb-0">IMD Dev</h1>
+                                <p>Desenvolvedor <code>back-end</code></p>
+                            </div>
+
+                        <img class="fluid clip-path" src="" alt="foto-dev">
+                            <div class="text-center">
+                                <h1 style="font-size: 1.4rem" class="mb-0">IMD Dev</h1>
+                                <p>Desenvolvedor <code>front-end</code></p>
+                            </div>
+
+                        <img class="fluid clip-path" src="" alt="foto-dev">
+                            <div class="text-center">
+                                <h1 style="font-size: 1.4rem" class="mb-0">IMD Dev</h1>
+                                <p>Desenvolvedor <code>front-end</code></p>
+                            </div>
+                </div>
+            </div>
+
+            
+
+                
+        </div>
+        
+        
+
+
+        
     </div>
-
-    <div class="col-12 col-sm-6 mt-4">
-        <h3>Docentes</h3>
-        <span class="span-info">Eugenio Paccelli Aguiar Freire</span>
-    </div>
-
-    <div class="col-12 col-sm-6 mt-4">
-        <h3>Discentes</h3>
-        <span class="d-block span-info">Pedro Gabriel Silva de Lima</span>
-        <span class="d-block span-info">Victor Simplício Brandão de Lima</span>
-    </div>
-
-        
-
-
-        
-        
-
 </div>
 @endsection
