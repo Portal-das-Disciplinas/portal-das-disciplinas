ee1298bb6711208fcf1f4b7efc6f2396c3d7ad48
henrymedeiros <henrymedeiros77@gmail.com>
2021-09-01 11:50:16 -0300

    fix: favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <meta name="robots" content="@yield('robots','all')">
     <title>@yield('title', 'Portal das Disciplinas IMD')</title>
     <meta name="description" content="@yield('description','Conheça as disciplinas do IMD de forma mais prática!')">
+    <link rel="shortcut icon" href="favicon.ico" />
     {{-- bootstrap CSS --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     {{-- CSS --}}
