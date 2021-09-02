e8fe6d716b14c66efc619523927b48fffd62a7e5
henrymedeiros <henrymedeiros77@gmail.com>
2021-09-02 10:17:05 -0300

    fix: removendo opcao de admin do user professor

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -56,8 +56,12 @@
               <div class="dropdown-menu">
                 <h6 class="dropdown-header">Olá, <span>{{ Str::words( Auth::user()->name, 2, '' ) }}</span><h6>
                 <div class="dropdown-divider"></div>
+                
                 <a href="{{route('profile')}}"><div class="dropdown-item py-3"> <i class="far fa-user mr-2"></i>  Meu perfil</div></a>
-                <a href="{{route('profile')}}"><div class="dropdown-item py-3"> <i class="fas fa-users-cog mr-2"></i>Painel de Administração</div></a>
+                @if (auth()->user()->is_admin)
+                <a href="{{ route("professores.index") }}"><div class="dropdown-item py-3"> <i class="fas fa-users-cog mr-2"></i>Painel de Administração</div></a>
+                @endif
+                
               </div>
             </div>
           @endauth
