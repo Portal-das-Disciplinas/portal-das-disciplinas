283eb0b94ced99d3ef326c766b6f9de699277e1f
henrymedeiros <henrymedeiros77@gmail.com>
2021-10-07 00:01:14 -0300

    feat: botão de cadastro no dropdown do menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -53,6 +53,7 @@
                 <div class="dropdown-divider"></div>
                 
                 <a href="{{route('profile')}}"><div class="dropdown-item py-3"> <i class="far fa-user mr-2"></i>  Meu perfil</div></a>
+                <a href="{{ route("disciplinas.create") }}"><div class="dropdown-item py-3"> <i class="fas fa-book mr-2"></i></i>  Cadastrar disciplina </div></a>
                 @if (auth()->user()->is_admin)
                 <a href="{{ route("professores.index") }}"><div class="dropdown-item py-3"> <i class="fas fa-users-cog mr-2"></i>Painel de Administração</div></a>
                 @endif
