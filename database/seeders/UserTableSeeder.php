c78033dceee623f41da177c8cf4fd272114b6e7e
Pedro Gabriel <pedrogab96@gmail.com>
2021-11-04 17:56:11 -0300

    fix completed

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -25,33 +25,33 @@ class UserTableSeeder extends Seeder
         $roleAdmin = Role::query()
             ->firstWhere('name', RoleName::ADMIN);
 
-        // User::create([
-        //     'name' => 'Victor',
-        //     'email' => 'victor_brandao@outlook.com',
-        //     'password' => '$2y$10$toyM29iTlElj.ShFuHu0KumYyhKXtSGXVhBHy.YzS9eSEg89sKQMW',
-        //     'role_id' => $roleAdmin->id,
-        // ]);
+        User::create([
+            'name' => 'Victor',
+            'email' => 'victor_brandao@outlook.com',
+            'password' => '$2y$10$toyM29iTlElj.ShFuHu0KumYyhKXtSGXVhBHy.YzS9eSEg89sKQMW',
+            'role_id' => $roleAdmin->id,
+        ]);
 
-        // User::create([
-        //     'name' => 'Pedro Gabriel',
-        //     'email' => 'pedrogab96@gmail.com',
-        //     'password' => '$2y$10$NHww673REnFjun6oaaChXub6MkPN.7WR//MNoUzZjh5ruqQTm7hIC',
-        //     'role_id' => $roleAdmin->id,
-        // ]);
+        User::create([
+            'name' => 'Pedro Gabriel',
+            'email' => 'pedrogab96@gmail.com',
+            'password' => '$2y$10$NHww673REnFjun6oaaChXub6MkPN.7WR//MNoUzZjh5ruqQTm7hIC',
+            'role_id' => $roleAdmin->id,
+        ]);
 
-        // User::create([
-        //     'name' => 'Arthur Sérvulo',
-        //     'email' => 'arthurservulo7@gmail.com',
-        //     'password' => '$2y$10$rBccdnryA5R5GbYzzen82uR2Sz7Iu7BB6Vi18ocIypjyP2wPn9PY2',
-        //     'role_id' => $roleAdmin->id,
-        // ]);
+        User::create([
+            'name' => 'Arthur Sérvulo',
+            'email' => 'arthurservulo7@gmail.com',
+            'password' => '$2y$10$rBccdnryA5R5GbYzzen82uR2Sz7Iu7BB6Vi18ocIypjyP2wPn9PY2',
+            'role_id' => $roleAdmin->id,
+        ]);
 
-        // User::create([
-        //     'name' => 'Eugenio Paccelli',
-        //     'email' => 'eugenio@admin.com',
-        //     'password' => '$2y$10$LwYZ050GVQXu.gksBsEXyubkImKd6xLyhaiZxl0PeeuOcaOeq/7/K',
-        //     'role_id' => $roleAdmin->id,
-        // ]);
+        User::create([
+            'name' => 'Eugenio Paccelli',
+            'email' => 'eugenio@admin.com',
+            'password' => '$2y$10$LwYZ050GVQXu.gksBsEXyubkImKd6xLyhaiZxl0PeeuOcaOeq/7/K',
+            'role_id' => $roleAdmin->id,
+        ]);
 
         User::create([
             'name' => 'Cristian',
@@ -81,18 +81,18 @@ class UserTableSeeder extends Seeder
          */
         $roleProfessor = Role::query()
             ->firstWhere('name', RoleName::PROFESSOR);
-        // $user = User::create([
-        //     'name' => 'Eugenio Paccelli',
-        //     'email' => 'eugenio@imd.ufrn.br',
-        //     'password' => '$2y$10$LwYZ050GVQXu.gksBsEXyubkImKd6xLyhaiZxl0PeeuOcaOeq/7/K',
-        //     'role_id' => $roleProfessor->id,
-        // ]);
-        // Professor::create([
-        //     'name' => $user->name,
-        //     'public_email' => $user->email,
-        //     'profile_pic_link' => '',
-        //     'user_id' => $user->id,
-        // ]);
+        $user = User::create([
+            'name' => 'Eugenio Paccelli',
+            'email' => 'eugenio@imd.ufrn.br',
+            'password' => '$2y$10$LwYZ050GVQXu.gksBsEXyubkImKd6xLyhaiZxl0PeeuOcaOeq/7/K',
+            'role_id' => $roleProfessor->id,
+        ]);
+        Professor::create([
+            'name' => $user->name,
+            'public_email' => $user->email,
+            'profile_pic_link' => '',
+            'user_id' => $user->id,
+        ]);
 
         if (app()->isLocal()) {
             $user = User::create([
