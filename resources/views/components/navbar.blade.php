a9d3af3ca6fbd7e51be24b6ca189aeaadd35e254
Cristian Soares <criricyt@gmail.com>
2022-02-18 16:19:44 -0300

    retirar menu hamburguer

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -39,11 +39,7 @@
             @endguest
           </div>
 
-        
-          
-
-
-          
+  
 
           <label class="label-btn d-none">
             <i class="fas fa-bars" id='navbar_btn' onclick="move(true)"></i>
@@ -92,40 +88,4 @@
     </nav>
 </header>
 
-@section('scripts-bottom')
-<script>
-  class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu)
-        this.navList = document.querySelector(navList)
-        this.navLinks = document.querySelectorAll(navLinks)
-        this.activeClass = "active"
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass)
-    }
-
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick)
-    }
-
-    init() {
-        if(this.mobileMenu) {
-            this.addClickEvent()
-        }
-        return this
-    }
-}
-
-const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list a",
-);
-
-mobileNavbar.init()
-</script>
-@endsection
 
