6aec10f7c6fa7ef5d9c65b42719bda75238d7a31
henrymedeiros <henrymedeiros77@gmail.com>
2022-03-12 20:21:57 -0300

    fix: painel e cadastros responsivos

--- resources/views/admin/classification/form.blade.php
+++ resources/views/admin/classification/form.blade.php
@@ -13,75 +13,79 @@ Cadastro de Professor
     <div class="page-title">
         <h1>Cadastrar classificação</h1>
     </div>
-    <h4 class="text-center m-4"> {{ isset($edit) ? 'Atualizar' : 'Cadastrar'}} Classificação</h4>
-     <form action="{{ isset($edit) ? route('classificacoes.update', $id) :  route('classificacoes.store') }}" method="post">
-        @csrf
-        @if(isset($edit) && $edit)
-            @method('PUT')
-        @endif
-        <div class="form-row">
-            <div class="form-group col-6">
-                <label for="name" class="text-black">Nome</label>
-                <input  type="text"
-                        class="form-control {{ $errors->has('name') ? 'is-invalid' : ''}}"
-                        name="name"
-                        id="name"
-                        placeholder="Nome da classificação"
-                        autocomplete="off"
-                        value="{{ $classification->name ?? '' }}">
-                @error('name')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="type_a" class="text-black">Tipo A</label>
-                <input  type="text"
-                        class="form-control {{ $errors->has('type_a') ? 'is-invalid' : ''}}"
-                        name="type_a"
-                        id="type_a"
-                        placeholder="Primeiro tipo da classificação"
-                        autocomplete="off"
-                        value="{{ $classification->type_a ?? '' }}">
-                    @error('type_a')
+
+    <div class="page-content" style='min-height: 60vh'>
+        <form action="{{ isset($edit) ? route('classificacoes.update', $id) :  route('classificacoes.store') }}" method="post">
+            @csrf
+            @if(isset($edit) && $edit)
+                @method('PUT')
+            @endif
+            <div class="form-row">
+                <div class="form-group col-12">
+                    <label for="name" class="text-black">Nome</label>
+                    <input  type="text"
+                            class="form-control {{ $errors->has('name') ? 'is-invalid' : ''}}"
+                            name="name"
+                            id="name"
+                            placeholder="Nome da classificação"
+                            autocomplete="off"
+                            value="{{ $classification->name ?? '' }}">
+                    @error('name')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
+                </div>
             </div>
-        </div>
-
-
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="type_b" class="text-black">Tipo B</label>
-                <input  type="text"
-                        class="form-control {{ $errors->has('type_b') ? 'is-invalid' : ''}}"
-                        name="type_b"
-                        id="type_b"
-                        placeholder="Segundo tipo da classificação"
-                        autocomplete="off"
-                        value="{{ $classification->type_b ?? '' }}">
-                @error('type_b')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
+            <div class="form-row">
+                <div class="form-group col-6">
+                    <label for="type_a" class="text-black">Tipo A</label>
+                    <input  type="text"
+                            class="form-control {{ $errors->has('type_a') ? 'is-invalid' : ''}}"
+                            name="type_a"
+                            id="type_a"
+                            placeholder="Primeiro tipo da classificação"
+                            autocomplete="off"
+                            value="{{ $classification->type_a ?? '' }}">
+                        @error('type_a')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                </div>
+                <div class="form-group col-6">
+                    <label for="type_b" class="text-black">Tipo B</label>
+                    <input  type="text"
+                            class="form-control {{ $errors->has('type_b') ? 'is-invalid' : ''}}"
+                            name="type_b"
+                            id="type_b"
+                            placeholder="Segundo tipo da classificação"
+                            autocomplete="off"
+                            value="{{ $classification->type_b ?? '' }}">
+                    @error('type_b')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
             </div>
-        </div>
-        
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="description" class="text-black">Descrição</label>
-                <textarea class="form-control {{ $errors->has('description') ? 'is-invalid' : ''}} " id="description" name="description"  rows="5"> {{ $classification->description ?? '' }}</textarea>
-                @error('description')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
+    
+    
+            <div class="form-row">
+                
             </div>
-        </div>
-
-        <div class="form-row">
-            <div class="form-group col-md-4 mt-2">
-                <input type="submit" value="{{ isset($edit) ? 'Atualizar' : 'Cadastrar'}}" class="btn btn-block btn-primary w-50">
+            
+            <div class="form-row">
+                <div class="form-group col-12">
+                    <label for="description" class="text-black">Descrição</label>
+                    <textarea class="form-control {{ $errors->has('description') ? 'is-invalid' : ''}} " id="description" name="description"  rows="5"> {{ $classification->description ?? '' }}</textarea>
+                    @error('description')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
             </div>
-        </div>
-     </form>
+    
+            <div class="form-row">
+                <div class="form-group col-12 mt-2">
+                    <input type="submit" value="{{ isset($edit) ? 'Atualizar' : 'Cadastrar'}}" class="btn btn-block btn-primary w-100">
+                </div>
+            </div>
+         </form>
+    </div>
+     
 </div>
 @endsection
