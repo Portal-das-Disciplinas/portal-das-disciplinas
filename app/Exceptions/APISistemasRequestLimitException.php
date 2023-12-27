736b55166a3010516accfb94216614d5faafe67f
andreciencomp <andreciencomp@gmail.com>
2023-12-27 10:34:58 -0300

    Alteração da documentação das classes de Exceção.

--- app/Exceptions/APISistemasRequestLimitException.php
+++ app/Exceptions/APISistemasRequestLimitException.php
@@ -4,6 +4,10 @@ namespace App\Exceptions;
 
 use Exception;
 
+/**
+ * Exceção lançada quando o limite de requisições por hora à API Sistemas é atingido.
+ * Este limite é de 5.000 requisições por hora.
+ */
 class APISistemasRequestLimitException extends Exception
 {
     //
--- app/Exceptions/APISistemasServerErrorException.php
+++ app/Exceptions/APISistemasServerErrorException.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Exception;
 
+/**
+ * Exceção lançada quando ocorre um erro de servidor na API Sistemas.
+ */
 class APISistemasServerErrorException extends Exception
 {
     //
