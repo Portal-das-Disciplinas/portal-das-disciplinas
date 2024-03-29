faf1b22a77ad1d4081d76bc2cb606e9b9babd35f
tiago-rodrigues1 <tiagords127@gmail.com>
2024-03-28 22:34:31 -0300

    criada migration topics

---
+++ database/migrations/2024_03_26_194323_create_topics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->enum('required_level', ['Aceitável', 'Bom', 'Ótimo', 'Notável', 'Excepcional'])->nullable();
+            $table->unsignedBigInteger('parent_topic_id')->nullable();
+            $table->foreign('parent_topic_id')->references('id')
+                ->on('topics')
+                ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
