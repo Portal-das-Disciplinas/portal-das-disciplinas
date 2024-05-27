e9f67aa70d4418b9912b8ed62c9701a5d9dd941c
tiago-rodrigues1 <tiagords127@gmail.com>
2024-05-27 11:37:28 -0300

    refactor subtopics

--- resources/views/components/subtopics.blade.php
+++ resources/views/components/subtopics.blade.php
@@ -3,13 +3,21 @@
         <li class="mb-3" id="topic-{{ $subtopic->id }}">
             <span class="topic-title" style="font-weight: normal;">{{ $subtopic->title }}</span>
 
-            @if (count($subtopic->subtopics) > 0)
+            @if (count($subtopic->subtopics) > 0 && Route::currentRouteName() != "disciplines.edit")
                 <a 
                     class="ml-3 expand-topic" 
                     data-topic_id="{{ $subtopic->id }}" 
                     style="cursor: pointer; font-size: 14px;"
                 >
                     Subtópicos
+                </a>
+            @else
+                <a 
+                    class="ml-3 expand-topic" 
+                    data-topic_id="{{ $topic->id }}" 
+                    style="cursor: pointer; font-size: 14px;"
+                >
+                    Mostrar mais
                 </a>
             @endif
 
