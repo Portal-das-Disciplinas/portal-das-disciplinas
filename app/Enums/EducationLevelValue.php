775dca1ea9bb30ced56f0787226ef3da9fcad538
andreciencomp <andreciencomp@gmail.com>
2025-01-07 11:02:18 -0300

    Alteração no seeder EducationLevelSeeder para incluir todos os níveis de ensino.

--- app/Enums/EducationLevelValue.php
+++ app/Enums/EducationLevelValue.php
@@ -5,6 +5,17 @@ namespace App\Enums;
 use App\Enums\Enum;
 
 class EducationLevelValue extends Enum{
+    const INFANTIL = "INFANTIL";
+    const FUNDAMENTAL = "FUNDAMENTAL";
+    const MEDIO= "MÉDIO";
+    const TECNICO = "TÉCNICO";
+    const INTEGRADO = "INTEGRADO";
+    const FORMACAO_COMPLEMENTAR = "FORMAÇÃO COMPLEMENTAR";
+    const GRADUACAO = "GRADUAÇÃO";
+    const LATO_SENSU_ESPECIALIZACAO = "ESPECIALIZAÇÃO";
+    const LATO_SENSU_RESIDENCIA = "RESIDÊNCIA";
+    const STRICTO_SENSU = "STRICTO SENSU";
+    const MESTRADO = "MESTRADO";
+    const DOUTORADO = "DOUTORADO";
 
-    const GRADUATION = 'GRADUAÇÃO';
 }
--- database/seeders/EducationLevelSeeder.php
+++ database/seeders/EducationLevelSeeder.php
@@ -16,8 +16,65 @@ class EducationLevelSeeder extends Seeder
     public function run()
     {
         EducationLevel::create([
-            'value' => EducationLevelValue::GRADUATION,
+            'value' => EducationLevelValue::INFANTIL,
+            'priority_level' => 1
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::FUNDAMENTAL,
+            'priority_level' => 2
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::MEDIO,
+            'priority_level' => 3
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::TECNICO,
+            'priority_level' => 4
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::INTEGRADO,
+            'priority_level' => 5
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::FORMACAO_COMPLEMENTAR,
+            'priority_level' => 6
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::GRADUACAO,
+            'priority_level' => 7
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::LATO_SENSU_ESPECIALIZACAO,
+            'priority_level' => 8
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::LATO_SENSU_RESIDENCIA,
+            'priority_level' => 9
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::STRICTO_SENSU,
             'priority_level' => 10
         ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::MESTRADO,
+            'priority_level' => 11
+        ]);
+
+        EducationLevel::create([
+            'value' => EducationLevelValue::DOUTORADO,
+            'priority_level' => 12
+        ]);
+
+
     }
 }
